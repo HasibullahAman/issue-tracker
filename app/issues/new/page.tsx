912da6c026960f521d98cb5d3958b42b4cc3da0e
HasibullahAman <hasib.aman7@gmail.com>
2025-07-24 02:18:40 +0430

    Refector Code, move submiting the code out of, and done 2nd Unite

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -35,6 +35,16 @@ const NewIssuePage = () => {
     });
     const [error, setError] = useState('');
     const [isSubmitting, setSubmitting] = useState(false);
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setSubmitting(false)
+            setError('An unexpected error occurred.')
+        }
+    })
     return (
         <div className='max-w-xl'>
             {error && <Callout.Root color='red' className='mb-5'>
@@ -47,16 +57,7 @@ const NewIssuePage = () => {
             </Callout.Root>
             }
             <form className='space-y-3'
-                onSubmit={handleSubmit(async (data) => {
-                    try {
-                        setSubmitting(true);
-                        await axios.post('/api/issues', data);
-                        router.push('/issues');
-                    } catch (error) {
-                        setSubmitting(false)
-                        setError('An unexpected error occurred.')
-                    }
-                })}
+                onSubmit={onSubmit}
             >
                 <TextField.Root placeholder="Add Title...!" {...register("title")}>
                 </TextField.Root>
